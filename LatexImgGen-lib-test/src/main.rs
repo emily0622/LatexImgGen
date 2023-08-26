e4f4cefdba88fa313ca67448b3b87366ec4c0904
Emily Traynor <emily.traynor@mail.utoronto.ca>
2023-08-26 19:44:25 -0400

    cleaned up test file

--- LatexImgGen-lib-test/src/main.rs
+++ LatexImgGen-lib-test/src/main.rs
@@ -1,8 +1,6 @@
 #![allow(non_snake_case)]
 extern crate LatexImgGen_lib;
-// use LatexImgGen_lib::LatexImgGen::play;
 use LatexImgGen_lib::LatexImgGen;
-// use dioxus::prelude::*;
 use std::assert;
 use std::path::Path;
 
@@ -15,11 +13,6 @@ pub fn main() {
 
     println!("Starting Test 2");
     test2();
-    // println!("Starting Test 2");
-
-//    test1();
-//    println!("Test2");
-//    play("Tutorialspoint".to_string())
 }
 
 
@@ -56,37 +49,3 @@ pub fn test2() {
     assert!(!(Path::new("../imgfolder/test2_0.svg").exists()),"Prev image not removed");
     println!("TEST2 PASSED");
 }
-
-
-// pub fn test2() {
-//     dioxus_desktop::launch(App);
-// }
-
-    
-// fn App(cx: Scope) -> Element {
-
-//     let img_name = use_state(cx, || "test0.svg".to_string());
-//     let eq_input = use_state(cx, || "y=\\frac{1}{x}".to_string());
-//     let mut img_bool = use_state(cx, || 0);
-
-//     cx.render(rsx!{
-//         form {
-//             onsubmit: move |_| {
-//                 img_name.set(LatexImgGen::render(img_name,eq_input.to_string()));
-//                 img_bool.set(1);
-//                 },
-//             input {
-//                 value: "{eq_input}",
-//                 oninput: |event| eq_input.set(event.value.clone()),
-//                 println!("eq_input: {eq_input}"),
-//             }
-//             input {
-//                 r#type: "submit",
-//             }
-//         },
-//         if *img_bool == 1 {
-//             img { src: "{img_name}" }
-//         } 
-//     })
-
-// }
